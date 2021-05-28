e1f92de12d924243609167d4fa1a47050bb440e6
filip <filis220@student.liu.se>
2021-05-28 17:32:30 +0200

    Fixed errors Lab1

--- artCode.py
+++ artCode.py
@@ -1,12 +1,12 @@
-F=[2/7, 5/7]
-x=[1,1,0,1]
+F=[0, 0.6, 0.9, 1]
+x=[1,3,1,1]
 l=[0,0,0,0,0]
 u=[1,0,0,0,0]
-for i in range(1,len(x)):
-    l[i] = l[i-1] + (u[i-1]-l[i-1])*F[x[i]-1]
-    u[i] = l[i-1] + (u[i-1]-l[i-1])*F[x[i]]
-print(l)
-print(u)
-print(u[3]-l[3])
-#0.05 , 0.07538
-#0.02538
+for i in range(1,len(x)+1):
+    l[i] = l[i-1] + (u[i-1]-l[i-1])*F[x[i-1]-1]
+    u[i] = l[i-1] + (u[i-1]-l[i-1])*F[x[i-1]]
+print(l[len(x)])
+print(u[len(x)])
+print(u[len(x)]-l[len(x)])
+#print(l)
+#print(u)
